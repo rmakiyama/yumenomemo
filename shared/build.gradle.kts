ecc2bd800bddaf44e26a8c08a1529ffef81fc280
rmakiyama <rmakiyama.io@gmail.com>
2021-11-23 01:12:18 +0900

    add datetime dependency

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -27,6 +27,7 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation(libs.sqldelight.runtime)
+                implementation(libs.kotlinx.datetime)
             }
         }
         val commonTest by getting {
